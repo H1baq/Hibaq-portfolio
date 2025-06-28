732e52f7ff1541086562d48c2fd4981dbf3a9d2a
hibby <hibaqku7@gmail.com>
2025-06-28 09:25:32 +0300

    Add App.jsx root component

---
+++ hibaq-react-portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Academics from './Components/Academics'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Academics/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+  
+   
+    </div>
+  )
+}
+
+export default App
